ef44bc27dad25ef03f2232efdde05207845f5c3b
Keyo <allenkeyo0405@gmail.com>
2024-09-26 15:09:33 +0700

    View Data & Model

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,25 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Allen Keyo', 'title' => 'About']);
+    return view('about', ['title' => 'About Page'], ['nama' => 'Allen Keyo']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog page', 'posts' => Post::all()]);
+});
+Route::get('/posts/{slug}', function ($slug) {
+    $post = Post::find($slug); 
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact']);
+    return view('contact', ['title' => 'Contact Page']);
 });
